6f92c3392113a3959f1228e528ac387d9d53fd6f
Nicolas Cena <nicolasnahuelcena@gmail.com>
2022-09-30 10:13:23 +0200

    Restitution

--- src/navigator/stackNavigation.jsx
+++ src/navigator/stackNavigation.jsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { WelcomeUserScreen } from '../screens/WelcomeUserScreen';
 import { LoginScreen } from '../screens/LoginScreen';
 import { RegistryScreen } from '../screens/RegistryScreen';
+import { MeditationScreen } from '../screens/MeditationScreen';
 
 
 export const StackNavigation = () => {
@@ -11,9 +12,10 @@ export const StackNavigation = () => {
 
   return (
     <Stack.Navigator>
+        <Stack.Screen name='MeditationScreen' component={MeditationScreen} options={{ headerTransparent: true, headerBackTitle: 'Volver', headerTitle: ''}} />
         <Stack.Screen name='WelcomeUserScreen' component={WelcomeUserScreen} options={{ headerShown: false}} />
         <Stack.Screen name='LoginScreen' component={LoginScreen} options={{ headerTransparent: true, headerBackTitle: 'Volver', headerTitle: ''}} />
-        <Stack.Screen name='RegistryScreen' component={RegistryScreen} options={{ headerTransparent: true, headerBackTitle: 'Volver', headerTitle: ''}} />
+        <Stack.Screen name='RegistryScreen' component={RegistryScreen} options={{ headerTransparent: false, headerBackTitle: 'Volver', headerTitle: 'Holis'}} />
     </Stack.Navigator>
   )
 }
